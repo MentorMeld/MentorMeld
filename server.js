19b1fa042e7cef812e0f3f5ecae596778fdfbaee
StephenDK <stephendklein08@gmail.com>
2018-04-06 11:41:41 -0700

    Boiler plate

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-// Import express package
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-// Import passport package
-const passport = require('passport');
-
-// Database connection
-
-// Setup instance of express
-const app = (express);
-
-
-// Routes
-
-
-// PORT
+const routes = require("./routes");
+const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets
+app.use(express.static("client/build"));
+// Add routes, both API and view
+app.use(routes);
 
-// Setup server to listen 
+
+
+// Start the API server
 app.listen(PORT, function() {
-    console.log("App listening on localhost: " + PORT);
-})
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
