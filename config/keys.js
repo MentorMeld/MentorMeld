922c4cb3cd573d1aa16815cf2af0e7f3493fd7ed
StephenDK <stephendklein08@gmail.com>
2018-04-07 12:55:46 -0700

    passport setup

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// This file will hold the google passport oauth 
+// API keys.
+// add this file to the .gitignore 
+module.exports = {
+    googleClientID:
+    googleClientSecret:
+};
---
+++ scripts/passport.js
@@ -0,0 +1,4 @@
+// require passport
+const passport = require('passport');
+// require Google passport strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
