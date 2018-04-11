69ea96ec3bba71fa76ceffe9127a786640676cab
StephenDK <stephendklein08@gmail.com>
2018-04-10 18:50:15 -0700

    passport setup

--- scripts/passport.js
+++ scripts/passport.js
@@ -2,3 +2,22 @@
 const passport = require('passport');
 // require Google passport strategy
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// require api keys
+const keys = require('../config/keys');
+
+
+
+
+
+
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    // callback URL
+    callbackURL: '/auth/google/callback'
+}, function (accessToken, refreshToken, profile, done) {
+    // query database to find if user exists
+    
+}))
