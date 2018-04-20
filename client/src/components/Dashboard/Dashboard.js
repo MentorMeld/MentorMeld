f9d22734aba1874f05845d840346350ce2e2de44
StephenDK <stephendklein08@gmail.com>
2018-04-20 14:46:21 -0700

    Dashboard setup

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import React, { component } from 'react';
+import axios from 'axios';
+import './dashboard.css';
+
+
+class Dashboard extends Component {
+
+
+    getData() {
+
+    };
+
+    render() {
+        return(
+            <div>Dashboard</div>
+        )
+    }
+}
