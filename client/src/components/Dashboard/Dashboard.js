648f54108b4e2101c0acfec12c55ef90c6e845c9
Akshay Rauthan <akshay@Akshays-MacBook-Pro.local>
2018-04-20 16:35:40 -0700

    Post Data coming on the screen

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './dashboard.css';
+import Postresults from '../PostResults';
 
 
 class Dashboard extends Component {
     
-
+    state = {
+        data:[]
+    }
     getData() {
         axios.get('/api/dashboard')
         .then(res => {
-            console.log(res);
+            //console.log(res);
+            this.setState({
+                data: res.data
+            });
+            console.log(this.state.data);
         }).catch(error => {
             console.log(error);
         })
@@ -21,7 +28,10 @@ class Dashboard extends Component {
 
     render() {
         return(
-            <div>Dashboard</div>
+            <div>
+                <h3>Dashboard</h3>
+                <Postresults results = {this.state.data} />
+            </div>    
         )
     }
 }
