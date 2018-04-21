a7edef43dfdfddcfd793688271c96134078f421f
Ruhi Kumbhani <ruhi.kumbhani@gmail.com>
2018-04-21 00:11:37 -0700

    styling post update

--- client/src/components/PostResults/PostResults.js
+++ client/src/components/PostResults/PostResults.js
@@ -4,7 +4,7 @@ import "./PostResults.css";
 const PostResults = props => (
   <ul>
     {props.results.map(result => (
-      <li key={result.id}>
+      <li className="card" key={result.id}>
         <label> Name: <span>{result.name}</span></label>
         <label> Email: <span>{result.email}</span></label>
         <label> Title: <span>{result.title}</span></label>
