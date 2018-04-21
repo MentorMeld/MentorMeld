b3be6b99bc1de27300cd82fea92bc085089977d9
Ruhi Kumbhani <ruhi.kumbhani@gmail.com>
2018-04-20 23:34:40 -0700

    landing update

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -13,13 +13,16 @@ import akshay from '../../images/akshay.jpeg';
 const Landing = () => (
     <div>
         <h3>Our Team</h3>
+            <p>Jack, Stephen, Ruhi, Akshay</p>
             <div className="row">
                 <img className="profile" src={jack} alt="jack" />
-                <img className="profile" src={stephen} alt="jack" />
-                <img className="profile" src={ruhi} alt="jack" />
-                <img className="profile" src={akshay} alt="jack" />
+                <img className="profile" src={stephen} alt="stephen" />
+                <img className="profile" src={ruhi} alt="ruhi" />
+                <img className="profile" src={akshay} alt="akshay" />
             </div>
-        <h3>Our Roles</h3>
+        <h3>About MentorMeld</h3>
+            <p>MentorMeld is a social media site with the purpose of connecting mentors to mentees.</p>
+            <p>Do you know somebody that has changed majors or careers after spending years in a chosen field? What if seeing what a day or a few days in the life of somebody that actually works in a chosen field looks like? We offer the opportunity to “test drive” a career field before committing.</p>
             <p></p>
     </div>
 );
